b0637167798276197480531cf2fe359cfa030f0a
Amadou-Marong <marongamadou6@gmail.com>
2024-09-18 06:07:52 +0100

    rest framework authentication

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from .serializers import TeacherSerializer
 from . import models
@@ -10,9 +10,11 @@ from . import models
 class TeacherList(generics.ListCreateAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
