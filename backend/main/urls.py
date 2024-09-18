69c46401ef22c31ff5052bb649702b25d79ad594
Amadou-Marong <marongamadou6@gmail.com>
2024-09-18 05:55:37 +0100

    created views

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teachers/', views.TeacherView.as_view()),
+    path('teachers/', views.TeacherList.as_view()),
+    path('teachers/<int:pk>/', views.TeacherDetail.as_view()),
 ]
