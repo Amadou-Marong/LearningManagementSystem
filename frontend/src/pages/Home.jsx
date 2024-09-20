fd5b4a0a4ad6a8830cfd26b43cca6448e1da0d51
Amadou-Marong <marongamadou6@gmail.com>
2024-09-20 21:04:58 +0100

    frontend starter

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,93 @@
+import logo from '../assets/react.svg'
 const Home = () => {
   return (
+    <>
+    {/* Latest Courses */}
     <div className="container mt-4">
+        <h3 className='border-bottom p-1 mb-2'>Latest Courses</h3>
         <div className="row">
             <div className="col-md-3">
                 <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
+    {/* Popular Courses */}
+    <div className="container mt-4">
+        <h3 className='border-bottom p-1 mb-2'>Latest Courses</h3>
+        <div className="row">
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+            <div className="col-md-3">
+                <div className="card">
+                    <a href="#"><img src={logo} alt="course image" className='card-img-top'/></a>
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <a href="#" className="btn btn-primary">Course Title</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
