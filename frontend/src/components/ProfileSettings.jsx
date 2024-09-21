c93ac793c62c1ef3f909b741c68016b9734a6240
Amadou-Marong <marongamadou6@gmail.com>
2024-09-21 21:10:12 +0100

    profile settings component

--- frontend/src/components/ProfileSettings.jsx
+++ frontend/src/components/ProfileSettings.jsx
@@ -3,7 +3,7 @@ const ProfileSettings = () => {
     <div className="card">
       <h5 className="card-header">Profile Settings</h5>
       <div className="card-body">
-        <div className="mb-3 row">
+        {/* <div className="mb-3 row">
           <label className="col-sm-2 col-form-label">Email</label>
           <div className="col-sm-10">
             <input
@@ -11,6 +11,24 @@ const ProfileSettings = () => {
               className="form-control-plaintext"
               id="staticEmail"
               value="email@example.com"
+            />
+          </div>
+        </div> */}
+        <div className="mb-3 row">
+          <label className="col-sm-2 col-form-label">Full Name</label>
+          <div className="col-sm-10">
+            <input
+              type="text"
+              className="form-control"
+            />
+          </div>
+        </div>
+        <div className="mb-3 row">
+          <label className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input
+              type="email"
+              className="form-control"
             />
           </div>
         </div>
@@ -24,6 +42,7 @@ const ProfileSettings = () => {
             />
           </div>
         </div>
+        <button className="btn btn-primary">Update</button>
       </div>
     </div>
   );
