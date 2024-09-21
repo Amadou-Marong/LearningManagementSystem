f1ab72e8f2fc031d958b514266979e13cff900e3
Amadou-Marong <marongamadou6@gmail.com>
2024-09-21 19:17:20 +0100

    user dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,51 @@
+import { Link } from "react-router-dom"
+
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="container mt-4">
+        <div className="row">
+            <aside className="col-md-3">
+                <div className="card">
+                    <div className="list-group list-group-flush">
+                        <h5 className="card-header">Dashboard</h5>
+                        <Link to='/' className="list-group-item list-list-group-item-action">My Courses</Link>
+                        <Link to='/' className="list-group-item list-list-group-item-action">Favorite Courses</Link>
+                        <Link to='/' className="list-group-item list-list-group-item-action">Recommended Courses</Link>
+                        <Link to='/' className="list-group-item list-list-group-item-action">Profile Settings</Link>
+                        <Link to='/' className="list-group-item list-list-group-item-action">Change Password</Link>
+                        <Link to='/' className="list-group-item list-list-group-item-action text-danger">Logout</Link>
+                    </div>
+                </div>
+            </aside>
+            <section className="col-md-9">
+                <div className="card">
+                    <h5 className="card-header">My Courses</h5>
+                    <div className="card-body">
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Created By</th>
+                                <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table-group-divider">
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Php Development</td>
+                                <td><Link to='/'>Jhon Doe</Link></td>
+                                <td>
+                                    <button className="btn btn-danger">Delete</button>
+                                </td>
+                                </tr>
+                            </tbody>
+                            </table>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
   )
 }
 
